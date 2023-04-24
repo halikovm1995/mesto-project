0d8ad0b6af6d4ace55d0bbbdfd0ebf6f5d9763db
Magomed Khalikov <salehard@stoxe.ru>
2023-04-24 09:38:08 +0500

    24.04.2023

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const formElement = document.querySelector('#popup-profile');
-const profileButton = document.querySelector('.profile__edit-button')
-const nameInput = document.querySelector('#nameinput'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = document.querySelector('#jobinput');// Воспользуйтесь инструментом .querySelector()
+const profileButton = document.querySelector('.profile__edit-button') // переменная кнопки редактировать профиль
+const nameInput = document.querySelector('#nameinput'); // 
+const jobInput = document.querySelector('#jobinput');// 
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description')
+const formAddPhoto = document.querySelector('#popup-photo'); 
+const addPhotoButton = document.querySelector('.profile__add-button');
+const elementContainer = document.querySelector('.element__list')
+const itemTemplate = document.querySelector('#template-item').content;
+const popupClose = document.querySelector('#popup-close');
+popupClose.addEventListener('click', function() {
+    formElement.classList.remove('popup_opened');
 
+})
 const initialCards = [
     {
       name: 'Архыз',
@@ -32,15 +40,18 @@ const initialCards = [
     }
   ]; 
 
-profileButton.addEventListener('click', function(){ //добавление кнопки открытия попап профиля
+  
+  profileButton.addEventListener('click', function(){ //добавление кнопки открытия попап профиля
     formElement.classList.add('popup_opened')
  })
-const popupClose = document.querySelector('.popup__close-icon');
-popupClose.addEventListener('click', function() {
-    formElement.classList.remove('popup_opened');
 
-})
  
+addPhotoButton.addEventListener('click', function(){ //добавление кнопки открытия попап профиля
+  formAddPhoto.classList.add('popup_opened')
+})
+
+
+
 
 function handleFormSubmit(evt) {
     evt.preventDefault(); 
@@ -54,13 +65,14 @@ formElement.addEventListener('submit', handleFormSubmit);
 nameInput.value = profileName.textContent
 jobInput.value = profileDescription.textContent 
 
-function addItem(linkValue, nameValue){
-    const itemTemplate = document.querySelector('#template-element').textContent;
-    const itemElement = itemTemplate.querySelector('.element__item').cloneNode(true);
-    itemElement.querySelector('.element__image').src = linkValue;
-    itemElement.querySelector('.element__title').textContent = nameValue;
-    elementContainer.append(itemElement);
-}
+
+
+initialCards.forEach(function (element){
+  const itemElement = itemTemplate.querySelector('.element__item').cloneNode(true);
+  itemElement.querySelector('.element__image').src = element.link;
+  itemElement.querySelector('.element__title').textContent = element.name;
+  elementContainer.append(itemElement);
+});
 
 
 
